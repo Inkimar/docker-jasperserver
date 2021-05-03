52feb92b709f94dcef951248e32feeca11b6ea75
Ingimar Erlingsson <inkimar@gmail.com>
2021-05-03 14:18:41 +0200

    moved Dockerfile+depending files to sub-dir

---
+++ scripts/fetch-v7.8.0.sh
@@ -0,0 +1,1 @@
+wget https://sourceforge.net/projects/jasperserver/files/JasperServer/JasperReports%20Server%20Community%20edition%207.8.0/TIB_js-jrs-cp_7.8.0_bin.zip/download -O ./jasperserver_A.zip
